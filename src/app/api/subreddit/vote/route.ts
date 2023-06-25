b88de4fa87b11df387219266ecb1d926c07957f6
ahmed nasser <ahmednas197@gmail.com>
2023-06-25 20:36:12 +0300

    Added voting for posts backend logic

---
+++ src/app/api/subreddit/vote/route.ts
@@ -0,0 +1,75 @@
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { PostVoteValidator } from "@/lib/validators/vote";
+
+const CACHE_AFTER_UPVOTES = 1;
+
+export async function PATCH(req: Request) {
+  try {
+    const body = await req.json();
+    const { postId, voteType } = PostVoteValidator.parse(body);
+
+    const session = await getAuthSession();
+
+    if (!session?.user) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    const existingVote = await db.vote.findFirst({
+      where: {
+        userId: session.user.id,
+        postId: postId,
+      },
+    });
+
+    const post = await db.post.findUnique({
+      where: {
+        id: postId,
+      },
+      include: {
+        author: true,
+        votes: true,
+      },
+    });
+
+    if (!post) {
+      return new Response("Post not found", { status: 404 });
+    }
+
+    if (existingVote) {
+      if (existingVote.type === voteType) {
+        await db.vote.delete({
+          where: {
+            userId_postId: {
+              postId,
+              userId: session.user.id,
+            },
+          },
+        });
+        return new Response("ok");
+      }
+    } else {
+      await db.vote.update({
+        where: {
+          userId_postId: {
+            postId,
+            userId: session.user.id,
+          },
+        },
+        data: {
+          type: voteType,
+        },
+      });
+      // Recount the votes
+      const votesAmt = post.votes.reduce((acc, vote) => {
+        if (vote.type === "UP") return acc + 1;
+        if (vote.type === "DOWN") return acc - 1;
+        return acc;
+      }, 0);
+
+      if (votesAmt >= CACHE_AFTER_UPVOTES) {
+        const cachePayload = {};
+      }
+    }
+  } catch (error) {}
+}
