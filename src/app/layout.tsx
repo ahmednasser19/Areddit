fe6a51ae9fccf3b6174f5b49c4f36f9495fdee9d
ahmed nasser <ahmednas197@gmail.com>
2023-06-29 14:52:15 +0300

    minor naming fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ import '@/styles/globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Breadit',
+  title: 'Areadit',
   description: 'A Reddit clone built with Next.js and TypeScript.',
 }
 
